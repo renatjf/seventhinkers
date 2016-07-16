2fcc1755caf1eb3f5e1f6f6ca28f3e3a7e6df2c8
Renato Freitas <renatjf@gmail.com>
2016-07-16 17:27:58 -0300

    novas fotos galeria

--- site/img/eventos/resize/lista.php
+++ site/img/eventos/resize/lista.php
@@ -1,6 +1,6 @@
-<?php $files = glob("img/eventos/resize/*.jpg"); for ($i=1; $i<count($files); $i++) { $num = $files[$i]; echo '<a class="links-eventos" title="Eventos" href="'.$num.'" rel="shadowbox[galeriaMenu]"></a>'; } ?>
+<?php $files = glob("img/eventos/resize/*.*"); for ($i=1; $i<count($files); $i++) { $num = $files[$i]; echo '<a class="links-eventos" title="Eventos" href="'.$num.'" rel="shadowbox[galeriaMenu]"></a>'; } ?>
 
-<?php $files = glob("img/eventos/resize/*.jpg"); for ($i=1; $i<count($files); $i++) { $num = $files[$i]; echo '<a class="links-eventos" title="Eventos" href="'.$num.'" rel="shadowbox[galeriaMobile]"></a>'; } ?>
+<?php $files = glob("img/eventos/resize/*.*"); for ($i=1; $i<count($files); $i++) { $num = $files[$i]; echo '<a class="links-eventos" title="Eventos" href="'.$num.'" rel="shadowbox[galeriaMobile]"></a>'; } ?>
 
 
 
